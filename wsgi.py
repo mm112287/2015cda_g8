3399f49f8da6839e148fc188fdd2b0419bdc41fb
40223151 <40223151@gm.nfu.edu.tw>
2015-05-19 11:03:03 +0800

    md. index2 and mygeartest2

--- wsgi.py
+++ wsgi.py
@@ -70,6 +70,354 @@ class Hello(object):
     def hello(self, toprint="Hello World!"):
         return toprint
     @cherrypy.expose
+    def index(self, K=None, N=None, inp2=None):
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.0-20150301-090019/brython.js"></script>
+    <script src="/static/Cango2D.js" type="text/javascript"></script>
+    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">
+        協同產品設計，第八組<br/>
+        <a href="index2">40223124，袁丞宗的程式</a><br/>
+        <a href="index3">40223151，簡正斌的程式</a><br/>
+        其他尚未建立
+    <table border=3>
+    <tr>
+　　 <th><font size="4">小組名單</font></th>
+　　 <th><font size="4"></font></th>
+    </tr>
+　 <tr rowspan="2">
+　　 <th><font size='4' color='yellow' ><a href="http://cd0427-40223151.rhcloud.com/">40223151簡正斌</a> </font></th>
+　　 <th><font size="4"><a href="http://cd0427-40223110.rhcloud.com/">40223110王常浩</a></font></th>
+　 </tr>
+　 <tr>
+　　 <th><font size="4"><a href="http://cd0427-40223124.rhcloud.com/">40223124袁丞宗 </a></font></th>
+　　 <th><font size="4"><a href="http://cd0427-40223129.rhcloud.com/">40223129許家瑋 </a></font></th>
+    </tr>
+　 <tr>
+　　 <th><font size='4' color='yellow' ><a href="http://2015springcda-40223149.rhcloud.com/">40223149賴涵餘</a></font></th>
+　　 <th><font size="4">40223150謝俊宇 </font></th>
+    </tr>
+　 <tr>
+　　 <th><font size="4">40223145劉兆銓</font></th>
+　　 <th></th>
+    </tr>
+  
+        '''
+        return outstring
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def index3(self, K=None, N=None, inp2=None):
+        # 將標準答案存入 answer session 對應區
+        theanswer = random.randint(1, 100)
+        thecount = 0
+        # 將答案與計算次數變數存進 session 對應變數
+        cherrypy.session['answer'] = theanswer
+        cherrypy.session['count'] = thecount
+        # 印出讓使用者輸入的超文件表單
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.0-20150301-090019/brython.js"></script>
+    <script src="/static/Cango2D.js" type="text/javascript"></script>
+    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">        
+    <form method=\"post\" action=\"mygeartest2\">
+        <fieldset>
+        <legend>考試協同七個齒輪齒輪參數表單值:</legend>
+        齒數1:<br />
+        <input type=\"text\" name=\"N\"><br />
+        齒數2:<br />
+        <input list="ng1" name="ng1">
+        <datalist id="ng1">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br />
+        齒數3:<br />
+        <input list="ng2" name="ng2">
+        <datalist id="ng2">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br /><br />
+        齒數4: <br />
+        <input list="ng3" name="ng3">
+        <datalist id="ng3">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br />
+        齒數5:<br />
+        <input list="ng4" name="ng4">
+        <datalist id="ng4">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br />
+        齒數6:<br />
+        <input list="ng5" name="ng5">
+        <datalist id="ng5">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br />
+        齒數7:<br />
+        <input list="ng6" name="ng6">
+        <datalist id="ng6">
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        <option value="30">30</option>
+        <option value="35">35</option>
+        <option value="40">40</option>
+        <option value="45">45</option>
+        <option value="50">50</option>
+        </datalist><br /><br />
+        模數:<br />
+        <input type=\"text\" name=\"K\"><br />
+        壓力角:<br />
+        <input type=\"text\" name=\"inp2\"><br />
+        <input type=\"submit\" value=\"確定\">
+        <input type=\"reset\" value=\"重填\">'''+self.menuLink()+'''
+    </form>
+    </body>
+    </html>
+    '''
+        return outstring
+    index3.exposed = True
+
+    #@+node:2015.20150330144929.1713: *3* twoDgear
+    @cherrypy.expose
+    # N 為齒數, M 為模數, P 為壓力角
+    def mygeartest2(self, K=None, N=None,ng1=None, ng2=None, ng3=None, ng4=None, ng5=None, ng6=None, inp2=None):
+        outString = ""
+        outString +="齒數1:"+N
+        outString += "<br />"
+        outString +="齒數2:"+ng1
+        outString += "<br />"
+        outString +="齒數3:"+ng2
+        outString += "<br />"
+        outString +="齒數4:"+ng3
+        outString += "<br />"
+        outString +="齒數5:"+ng4
+        outString += "<br />"
+        outString +="齒數6:"+ng5
+        outString += "<br />"
+        outString +="齒數7:"+ng6
+        outString += "<br />"
+        outString +="模數:"+K
+        outString += "<br />"
+        outString +="壓力角:"+inp2
+        outString += "<br />"
+        outString += self.menuLink()
+        outString += '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <!-- 載入 brython.js -->
+    <script type="text/javascript" src="/static/Brython3.1.1-20150328-091302/brython.js"></script>
+    <script src="/static/Cango2D.js" type="text/javascript"></script>
+    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
+    </head>
+    <!-- 啟動 brython() -->
+    <body onload="brython()">
+
+    <!-- 以下為 canvas 畫圖程式 -->
+    <script type="text/python">
+    # 從 browser 導入 document
+    from browser import document
+    from math import *
+    # 請注意, 這裡導入位於 Lib/site-packages 目錄下的 spur.py 檔案
+    import spur
+
+    # 準備在 id="plotarea" 的 canvas 中繪圖
+    canvas = document["plotarea"]
+    ctx = canvas.getContext("2d")
+    # 以下利用 spur.py 程式進行繪圖, 接下來的協同設計運算必須要配合使用者的需求進行設計運算與繪圖
+    # 其中並將工作分配給其他組員建立類似 spur.py 的相關零件繪圖模組
+    # midx, midy 為齒輪圓心座標, rp 為節圓半徑, n 為齒數, pa 為壓力角, color 為線的顏色
+    # Gear(midx, midy, rp, n=20, pa=20, color="black"):
+    # 模數決定齒的尺寸大小, 囓合齒輪組必須有相同的模數與壓力角
+    # 壓力角 pa 單位為角度
+    pa = '''+str(inp2)+'''
+    # m 為模數
+    m = '''+str(K)+'''
+    # 第1齒輪齒數
+    n_g1 = '''+str(N)+'''
+    # 第2齒輪齒數
+    n_g2 = '''+str(ng1)+'''
+    n_g3 = '''+str(ng2)+'''
+    n_g4 = '''+str(ng3)+'''
+    n_g5 = '''+str(ng4)+'''
+    n_g6 = '''+str(ng5)+'''
+    n_g7 = '''+str(ng6)+'''
+    # 計算兩齒輪的節圓半徑
+    rp_g1 = m*n_g1/2
+    rp_g2 = m*n_g2/2
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
+    rp_g5 = m*n_g5/2
+    rp_g6 = m*n_g6/2
+    rp_g7 = m*n_g7/2
+ #齒輪嚙合的旋轉角
+# 將第1齒輪順時鐘轉 90 度
+    th1 = pi/2
+
+    # 將第2齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    th2 = -pi/2-pi/n_g2
+
+    # 將第3齒輪逆時鐘轉 90 度之後, 再往回轉第2齒輪定位帶動轉角, 然後再逆時鐘多轉一齒, 以便與第2齒輪進行囓合
+    # 第1個 -pi/2 為將原先垂直的第3齒輪定位線逆時鐘旋轉 90 度
+    # -pi/n_g3 則是第3齒與第2齒定位線重合後, 必須再逆時鐘多轉一齒的轉角, 以便進行囓合
+    # (pi+pi/n_g2)*n_g2/n_g3 則是第2齒原定位線為順時鐘轉動 90 度, 
+    # pi+pi/n_g2 為第2齒輪從順時鐘轉 90 度之後, 必須配合目前的標記線所作的齒輪 2 轉動角度, 要轉換到齒輪3 的轉動角度
+    # 必須乘上兩齒輪齒數的比例, 若齒輪2 大, 則齒輪3 會轉動較快
+    # 但是第2齒輪為了與第1齒輪囓合, 已經距離定位線, 多轉了 180 度, 再加上第2齒輪的一齒角度, 因為要帶動第3齒輪定位, 
+    # 這個修正角度必須要再配合第2齒與第3齒的轉速比加以轉換成第3齒輪的轉角, 因此乘上 n_g2/n_g3
+    th3 = -pi/2-pi/n_g3+(pi+pi/n_g2)*n_g2/n_g3
+
+    th4 = -pi/2-pi/n_g4+(pi+pi/n_g3)*n_g3/n_g4-(pi+pi/n_g2)*n_g2/n_g4
+    th5 = -pi/2-pi/n_g5+(pi+pi/n_g4)*n_g4/n_g5-(pi+pi/n_g3)*n_g3/n_g5+(pi+pi/n_g2)*n_g2/n_g5
+    th6 = -pi/2-pi/n_g6+(pi+pi/n_g5)*n_g5/n_g6-(pi+pi/n_g4)*n_g4/n_g6+(pi+pi/n_g3)*n_g3/n_g6-(pi+pi/n_g2)*n_g2/n_g6
+    th7 = -pi/2-pi/n_g7+(pi+pi/n_g6)*n_g6/n_g7-(pi+pi/n_g5)*n_g5/n_g7+(pi+pi/n_g4)*n_g4/n_g7-(pi+pi/n_g3)*n_g3/n_g7+(pi+pi/n_g2)*n_g2/n_g7
+
+
+    # 將第1齒輪順時鐘轉 90 度
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400,400)
+    # rotate to engage
+    ctx.rotate( th1)
+    # put it back
+    ctx.translate(-400,-400)
+    spur.Spur(ctx).Gear(400,400,rp_g1,n_g1, pa, "blue")
+    ctx.restore()
+    ctx.font = "10px Verdana";
+    ctx.fillText("組員:24號袁丞宗所繪製",400-60, 400-10);
+
+    # 將第2齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+rp_g2,400)
+    # rotate to engage
+    ctx.rotate( th2)
+    # put it back
+    ctx.translate(-(400+rp_g1+rp_g2),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+rp_g2,400,rp_g2,n_g2, pa, "black")
+    ctx.restore()
+
+    # 將第3齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+2*rp_g2+rp_g3,400)
+    # rotate to engage
+    ctx.rotate( th3)
+    # put it back
+    ctx.translate(-(400+rp_g1+2*rp_g2+rp_g3),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+rp_g3,400,rp_g3,n_g3, pa, "red")
+    ctx.restore()
+
+    # 將第4齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4,400)
+    # rotate to engage
+    ctx.rotate( th4)
+    # put it back
+    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4,400,rp_g4,n_g4, pa, "black")
+    ctx.restore()
+
+    # 將第5齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5,400)
+    # rotate to engage
+    ctx.rotate( th5)
+    # put it back
+    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5,400,rp_g5,n_g5 ,pa, "green")
+    ctx.restore()
+
+    # 將第6齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6,400)
+    # rotate to engage
+    ctx.rotate( th6)
+    # put it back
+    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6,400,rp_g6,n_g6 ,pa, "yellow")
+    ctx.restore()
+
+    # 將第7齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+2*rp_g6+rp_g7,400)
+    # rotate to engage
+    ctx.rotate( th7)
+    # put it back
+    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+2*rp_g6+rp_g7),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+2*rp_g6+rp_g7,400,rp_g7,n_g7 ,pa, "pink")
+    ctx.restore()
+    # 假如第3齒也要進行囓合, 又該如何進行繪圖?
+    #spur.Spur(ctx).Gear(400,400,100,12, pa, "red")
+
+    </script>
+    <canvas id="plotarea" width="3600" height="3600"></canvas>
+    </body>
+    </html>
+    '''
+
+        return outString
+    mygeartest2.exposed = True
+
+    #@-others
     def index2(self, K=None, N=None, inp2=None):
         # 將標準答案存入 answer session 對應區
         theanswer = random.randint(1, 100)
@@ -275,6 +623,7 @@ gear(400,400,300,41,"blue")
     '''
 
         return outstring
+    index2.exposed = True
     @cherrypy.expose
     # N 為齒數, M 為模數, P 為壓力角
     def mygeartest2(self, m=None, ng1=None, ng2=None, ng3=None, ng4=None, ng5=None, ng6=None, ng7=None, inp2=None):
@@ -656,28 +1005,7 @@ gear(0,400,'''+str(K)+''','''+str(N)+'''-2,"read")
     </form>'''
         return outstring0
     @cherrypy.expose
-    def index(self, K=None, N=None, inp2=None):
-        outstring = '''
-    <!DOCTYPE html> 
-    <html>
-    <head>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8">
-    <!-- 載入 brython.js -->
-    <script type="text/javascript" src="/static/Brython3.1.0-20150301-090019/brython.js"></script>
-    <script src="/static/Cango2D.js" type="text/javascript"></script>
-    <script src="/static/gearUtils-04.js" type="text/javascript"></script>
-    </head>
-    <!-- 啟動 brython() -->
-    <body onload="brython()">
-        協同產品設計，第八組<br/>
-        <a href="index2">40223124，袁丞宗的程式</a><br/>
-        <a href="http://cd0427-40223151.rhcloud.com/index">40223151，簡正斌的程式</a><br/>
-        其他尚未建立
-        
-        '''
-        return outstring
-    @cherrypy.expose
-    # N 為齒數, M 為模數, P 為壓力角
+    
     def twoDgear(self, N=20, M=5, P=15):
         outstring = '''
     <!DOCTYPE html> 
